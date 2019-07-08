0c501285554454ec6623240db42d52b7c2cab91b
ShadowFoxNixill <github@nix.nixill.net>
2019-07-08 02:40:10 -0400

    more work

--- src/main/java/net/nixill/dicebot/App.java
+++ src/main/java/net/nixill/dicebot/App.java
@@ -7,17 +7,30 @@ import java.nio.file.Paths;
 
 import discord4j.core.DiscordClient;
 import discord4j.core.DiscordClientBuilder;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.util.Snowflake;
 import net.nixill.dicebot.events.MessageListener;
 
 public class App {
+  private static Snowflake us = null;
+  
   public static void main(String[] args) throws IOException {
-    String key = new String(Files.readAllBytes(Paths.get("key.txt")), StandardCharsets.UTF_8);
-
+    String key = new String(Files.readAllBytes(Paths.get("key.txt")),
+        StandardCharsets.UTF_8);
+    
     final DiscordClient client = new DiscordClientBuilder(key).build();
-
-    client.getEventDispatcher().on(MessageCreateEvent.class).subscribe(new MessageListener());
-
+    
+    client.getEventDispatcher().on(MessageCreateEvent.class)
+        .subscribe(new MessageListener());
+    client.getEventDispatcher().on(ReadyEvent.class).subscribe((event) -> {
+      us = event.getSelf().getId();
+    });
+    
     client.login().block();
+  }
+  
+  public static Snowflake getOurId() {
+    return us;
   }
 }
--- src/main/java/net/nixill/dicebot/events/MessageListener.java
+++ src/main/java/net/nixill/dicebot/events/MessageListener.java
@@ -2,64 +2,70 @@ package net.nixill.dicebot.events;
 
 import java.util.Optional;
 import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import discord4j.core.event.domain.message.MessageCreateEvent;
 import discord4j.core.object.entity.Message;
-import discord4j.core.object.entity.MessageChannel;
-import discord4j.core.object.entity.User;
-import net.nixill.dice.objects.DCEntity;
-import net.nixill.dice.objects.DCExpression;
-import net.nixill.dice.parsing.ExpressionSplitter;
+import net.nixill.dicebot.App;
+import net.nixill.dicebot.commands.RollCommand;
 import reactor.core.publisher.Mono;
 
 public class MessageListener implements Consumer<MessageCreateEvent> {
+  private static Pattern ptnRoll = Pattern.compile(
+      "(?:(f(?:ull)?)|(d(?:ebug)?))?(s(?:eed(?:ed)?)?)?(?:r(?:oll)?|dice)");
+  
   public void accept(MessageCreateEvent t) {
     Message msg = t.getMessage();
     Optional<String> opt = msg.getContent();
-
+    
     if (!opt.isPresent()) {
       return;
     }
-
+    
     String contents = opt.get();
-
+    
     msg.getChannel().flatMap((channel) -> {
-      if (!(channel instanceof MessageChannel)) {
+      Matcher mtc = Pattern
+          .compile(
+              "((?:<@!?\\d+> )*<@!?\\d+>)? ?!([a-zA-Z0-9-_]+)(?: *(.+))?")
+          .matcher(contents);
+      
+      if (!mtc.matches()) {
         return Mono.empty();
       }
-
-      MessageChannel chan = (MessageChannel) channel;
-
-      if (contents.startsWith("!r ")) {
-        return chan.createMessage("The command has changed to `r!(your roll)` now.");
+      
+      boolean mention = false;
+      String mentions = mtc.group(1);
+      String command = mtc.group(2);
+      String args = mtc.group(3);
+      
+      // If the command makes any mentions, we must be included.
+      // If there are mentions but not us, return nothing.
+      // Also, some commands require mentions, so also return whether or
+      // not we are.
+      if (mentions != null) {
+        if (mentions.contains("<@" + App.getOurId().asString() + ">")
+            || mentions
+                .contains("<@!" + App.getOurId().asString() + ">")) {
+          mention = true;
+        } else {
+          return Mono.empty();
+        }
       }
-
-      if (!contents.startsWith("r!")) {
-        return Mono.empty();
+      
+      // Now start checking what command was used
+      
+      // Was it !r(oll)?
+      Matcher rollMatcher = ptnRoll.matcher(command);
+      
+      if (rollMatcher.matches()) {
+        return channel.createMessage(
+            RollCommand.of(msg, channel, rollMatcher, args));
       }
-
-      String roll = contents.substring(2);
-
-      if (roll.equals("help")) {
-        return chan.createMessage("Coming soon!");
-      }
-
-      DCEntity rollEnt = ExpressionSplitter.parse(roll);
-      Optional<User> usr = msg.getAuthor();
-      String out = "";
-      if (usr.isPresent()) {
-        out = usr.get().getMention() + ": ";
-      } else {
-        out = "(no user): ";
-      }
-
-      out += "Input: `" + rollEnt.toString() + "`";
-      if (rollEnt instanceof DCExpression) {
-        out += " / Result: " + rollEnt.getValue().toString();
-      }
-
-      return chan.createMessage(out);
+      
+      return Mono.empty();
     }).subscribe();
-
+    
   }
 }
